13cc61e9c1fdccd7eeee471b9aba307d231a38bd
Henrique Teixeira <ht.henrique@live.com>
2023-02-26 20:31:51 -0300

    feat: backend integration

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit, ViewChild} from '@angular/core';
 import {FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import { User } from 'src/app/model/user/user';
+import {LoginServiceService} from "../../service/login-service.service";
+import {ServiceResponse} from "../../model/service/service-response";
+import {DOCUMENT} from "@angular/common";
 
 @Component({
   selector: 'app-login',
@@ -15,14 +19,39 @@ export class LoginComponent implements OnInit{
   userpassHint: string = 'Password';
   forgotPass: string = 'Forgot password?';
   loginButtonHint: string = 'Enter';
-  errorMessage: string = 'Invalid Credentials';
+  errorMessage: string = '';
 
-  constructor(private router: Router) {}
+  errorMessageId: boolean = false;
+
+  user:User = new User();
+  serviceResponse:ServiceResponse = new ServiceResponse();
+
+
+  constructor(@Inject(DOCUMENT) document: Document, private router: Router, private loginService:LoginServiceService) {
+
+  }
 
   ngOnInit() {}
 
   onSubmit() {
     this.router.navigate(['main']);
+  }
+
+  userLogin(){
+    this.loginService.loginUser(this.user).subscribe((response:ServiceResponse) =>{
+
+      this.serviceResponse = response;
+      if (this.serviceResponse.message == "Success"){
+        this.goToPage();
+      }
+      if (this.serviceResponse.message == "Fail"){
+
+        this.errorMessageId = true;
+
+        this.errorMessage = this.serviceResponse.message;
+      }
+      console.log(response)
+    });
   }
 
   goToPage(){
---
+++ src/app/service/login-service.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {User} from "../model/user/user";
+import {Observable} from "rxjs";
+import {ServiceResponse} from "../model/service/service-response";
+import {environment} from "../../environments/environment";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginServiceService {
+
+  private baseUrl= `${environment.baseUrl}`;
+
+  constructor(private httpClient: HttpClient) { }
+
+  loginUser(user:User):Observable<any>{
+    return this.httpClient.post<ServiceResponse>(`${this.baseUrl}`+ "/mazebank/authenticate/user", user)
+  }
+}
